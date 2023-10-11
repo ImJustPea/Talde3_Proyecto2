72df4b2381f8e87b17b8d3c8aa87db8cd989e214
Meybell <jonlosada2@gmail.com>
2023-10-11 09:03:29 +0200

    #

--- js/actividades.js
+++ js/actividades.js
@@ -1,5 +1,5 @@
     var boton = document.getElementById("reservar");
-    boton.onclick.function(); {
-        window.location.href = "../Talde3_Proyecto2/html/formulario.html";
+    boton.onclick = function() {
+        window.location.href = "../html/formulario.html";
     
 }
